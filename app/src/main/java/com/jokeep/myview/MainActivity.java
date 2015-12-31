49117b8200aca3cbf483828275b23ec8293202ed
wbq501 <749937855@qq.com>
2015-12-31 15:37:38 +0800

    自定义水波纹

--- app/src/main/java/com/jokeep/myview/MainActivity.java
+++ app/src/main/java/com/jokeep/myview/MainActivity.java
@@ -10,7 +10,7 @@ import android.widget.Button;
 
 
 public class MainActivity extends ActionBarActivity implements View.OnClickListener{
-    Button btn1,btn2,btn3,btn4;
+    Button btn1,btn2,btn3,btn4,btn5;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -23,6 +23,8 @@ public class MainActivity extends ActionBarActivity implements View.OnClickListe
         btn3.setOnClickListener(this);
         btn4 = (Button) findViewById(R.id.btn4);
         btn4.setOnClickListener(this);
+        btn5 = (Button) findViewById(R.id.btn5);
+        btn5.setOnClickListener(this);
     }
 
     @Override
@@ -62,6 +64,9 @@ public class MainActivity extends ActionBarActivity implements View.OnClickListe
                 break;
             case R.id.btn4:
                 intent = new Intent(MainActivity.this,MyView4Activity.class);
+                break;
+            case R.id.btn5:
+                intent = new Intent(MainActivity.this,MyView5Activity.class);
                 break;
         }
         startActivity(intent);
---
+++ app/src/main/java/com/jokeep/myview/MyView5Activity.java
@@ -0,0 +1,16 @@
+package com.jokeep.myview;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by wbq501 on 2015-12-31 15:28.
+ * MyView
+ */
+public class MyView5Activity extends Activity{
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.myview5);
+    }
+}
